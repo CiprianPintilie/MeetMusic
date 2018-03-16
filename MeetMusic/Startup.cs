7265263a7bf58cea6cece4cb1bd970982f16c4e0
anasHaz <a.hazgour@gmail.com>
2018-03-16 10:57:15 +0100

    Commit
    No changes

--- MeetMusic/Startup.cs
+++ MeetMusic/Startup.cs
@@ -27,7 +27,7 @@ namespace MeetMusic
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("HelloO World!");
             });
         }
     }
