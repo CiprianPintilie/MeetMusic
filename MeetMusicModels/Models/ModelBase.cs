a2194ed6634b87c827892c7b8ae6ea1e101e9529
CiprianPintilie <pintilie.c.a@gmail.com>
2018-03-18 18:37:59 +0100

    New - Add model base and user model

---
+++ MeetMusicModels/Models/ModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MeetMusicModels.Models
+{
+    public abstract class ModelBase
+    {
+        protected ModelBase()
+        {
+            Id = new Guid().ToString();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+
+        public DateTime? DeletedAt { get; set; }
+
+        public bool Deleted { get; set; }
+    }
+}
--- MeetMusicModels/Models/User.cs
+++ MeetMusicModels/Models/User.cs
@@ -1,6 +1,45 @@
-﻿namespace MeetMusicModels.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetMusicModels.Models
 {
-    public class User
+    public class User : ModelBase
     {
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(32, MinimumLength = 6, ErrorMessage = "Password must contains between {2} & {1} characters.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [RegularExpression(@"^[a-zA-Z]{2,40}$",
+        ErrorMessage = "Firstname must be at least 2 characters and not exceed 40 characters")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [RegularExpression(@"^[a-zA-Z]{2,40}$",
+        ErrorMessage = "Lastname must be at least 2 characters and not exceed 40 characters")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Required")]
+        [RegularExpression(@"^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$",
+        ErrorMessage = "Not a mail adress")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [RegularExpression(@"^[1-2]$", ErrorMessage = "Gender must be an integer between 1 and 2")]
+        public int Gender { get; set; }
+
+        public string AvatarUrl { get; set; }
+
+        [RegularExpression(@"^(0|\+33)[1-9]([-. ]?[0-9]{2}){4}$",
+        ErrorMessage = "Not a valid phone number")]
+        public string PhoneNumber { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime BirthDate { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description is limited to {1} characters.")]
+        public string Description { get; set; }
     }
 }
