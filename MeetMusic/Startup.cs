b809f36e367825ebeace5012b2968b0e1af34290
CiprianPintilie <pintilie.c.a@gmail.com>
2018-03-18 18:42:13 +0100

    Change - Setup DB context

--- MeetMusic/Startup.cs
+++ MeetMusic/Startup.cs
@@ -1,16 +1,33 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Data.Context;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MeetMusic
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", reloadOnChange: true, optional: false)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", reloadOnChange: true, optional: true);
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MeetMusicDbContext>(options =>
+            {
+                options.UseMySQL(Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("MeetMusic"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
