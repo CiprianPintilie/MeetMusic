d994d1a109ce48f3d2d63ff5f7e98f4d2b7226f4
CiprianPintilie <pintilie.c.a@gmail.com>
2018-03-18 11:22:44 +0100

    New - Add data project (for db context and migrations)

---
+++ DataAccess/Context/MeetMusicDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Data.Provider;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Data.Context
+{
+    public class MeetMusicDbContext : DbContext
+    {
+        public string ConnectionString { get; set; }
+
+
+        public MeetMusicDbContext(DbContextOptions<MeetMusicDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder); var fact = new LoggerFactory();
+            fact.AddProvider(new SqlLoggerProvider());
+            optionsBuilder.UseLoggerFactory(fact);
+        }
+    }
+}
---
+++ DataAccess/Provider/SqlLoggerProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace Data.Provider
+{
+    public class SqlLoggerProvider : ILoggerProvider
+    {
+        public void Dispose()
+        {
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new SqlLoger();
+        }
+
+        private class SqlLoger : ILogger
+        {
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return null;// throw new NotImplementedException();
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                //throw new NotImplementedException();
+                Debug.WriteLine($"SQL: ${formatter(state, exception)}");
+            }
+        }
+    }
+}
