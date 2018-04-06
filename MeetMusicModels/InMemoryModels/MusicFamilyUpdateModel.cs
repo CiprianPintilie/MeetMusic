f3088199f2f418684683ece3df993e265af0f2c9
CiprianPintilie <pintilie.c.a@gmail.com>
2018-04-06 11:16:15 +0200

    Meh

---
+++ MeetMusicModels/InMemoryModels/MusicFamilyUpdateModel.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace MeetMusicModels.InMemoryModels
+{
+    public class MusicFamilyUpdateModel
+    {
+        [JsonProperty("genre")]
+        public string GenreName { get; set; }
+        [JsonProperty("family")]
+        public string FamilyName { get; set; }
+    }
+}
